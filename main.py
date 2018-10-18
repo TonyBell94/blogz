60295c8ad69149c32a2bd51254a6e0e7292bce6b
TonyBell94 <TonyBell94@gmail.com>
2018-10-18 01:36:49 -0400

    added new files and login button

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:beproductive@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -15,6 +15,7 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     body = db.Column(db.String(900))
+    
 
     def __init__(self, name, body):
         self.name = name
@@ -63,6 +64,17 @@ def display():
 
 
     return render_template('todos.html')
+
+@app.route('/login')
+def login():
+    Username = request.form['Username']
+    Password = request.form['Password']
+    #if password == password:
+    print(Username)
+    print(Password)
+    flash("Successful Logged In")
+    return render_template('login.html')
+    
 
 
 if __name__ == '__main__':
